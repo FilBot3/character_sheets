953d99a1094c3a08ef5e84724101b14a2acd9559
Phillip Dudley <Predatorian3@gmail.com>
2018-10-09 22:04:01 -0500

    Getting the directories setup

---
+++ genesys_core_system/generate_character.rb
@@ -0,0 +1,21 @@
+#
+
+require 'erb'
+require 'yaml'
+
+if ARGV[0].nil? || ARGV[0].empty?
+  raise "Supply the YAML character data file."
+end
+
+character_data_file = ARGV[0]
+
+character_vars = YAML.safe_load(File.read(character_data_file))
+
+template = ERB.new(File.read('character_templates/ffg_genesys_core_system_character_sheet.html.erb'), nil, "%")
+#template.run
+
+File.open("character_sheets/#{character_vars['character_name']}_genesys_core_system_character_sheet.html", 'w') do |file|
+  file.write(template.result(binding))
+end
+
+puts "=> Generated character_sheets/#{character_vars['character_name']}_genesys_core_system_character_sheet.html"
--- star_wars_edge_of_the_empire/generate_character.rb
+++ star_wars_edge_of_the_empire/generate_character.rb
@@ -11,11 +11,11 @@ character_data_file = ARGV[0]
 
 character_vars = YAML.safe_load(File.read(character_data_file))
 
-template = ERB.new(File.read('./ffg_star_wars_edge_of_the_empire_character_sheet.html.erb'), nil, "%")
+template = ERB.new(File.read('character_templates/ffg_star_wars_edge_of_the_empire_character_sheet.html.erb'), nil, "%")
 #template.run
 
-File.open("#{character_vars['character_name']}_star_wars_eote_character_sheet.html", 'w') do |file|
+File.open("character_sheets/#{character_vars['character_name']}_star_wars_eote_character_sheet.html", 'w') do |file|
   file.write(template.result(binding))
 end
 
-puts "=> Generated #{character_vars['character_name']}_star_wars_eote_character_sheet.html"
+puts "=> Generated character_sheets/#{character_vars['character_name']}_star_wars_eote_character_sheet.html"
