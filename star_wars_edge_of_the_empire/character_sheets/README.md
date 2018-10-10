b6ea2707352aa725f9f9128f17b691776a943462
Phillip Dudley <Predatorian3@gmail.com>
2018-10-09 21:47:47 -0500

    Made all the files work for EotE

---
+++ star_wars_edge_of_the_empire/character_sheets/README.md
@@ -0,0 +1,3 @@
+# Finalized Character Sheets
+
+This directory holds the finalized Character Sheets for Edge of the Empire.
---
+++ star_wars_edge_of_the_empire/generate_character.rb
@@ -0,0 +1,21 @@
+#
+
+require 'erb'
+require 'yaml'
+
+if ARGV[0].nil? || ARGV[0].empty?
+  raise "Supply the YAML character data file."
+end
+
+character_data_file = ARGV[0]
+
+character_vars = YAML.safe_load(File.read(character_data_file))
+
+template = ERB.new(File.read('./ffg_star_wars_edge_of_the_empire_character_sheet.html.erb'), nil, "%")
+#template.run
+
+File.open("#{character_vars['character_name']}_star_wars_eote_character_sheet.html", 'w') do |file|
+  file.write(template.result(binding))
+end
+
+puts "=> Generated #{character_vars['character_name']}_star_wars_eote_character_sheet.html"
